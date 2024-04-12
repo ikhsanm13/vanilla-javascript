e225e6e0b50b0797aa12e42b227978c1284642e5
Ikhsan <mohamadichsan13@gmail.com>
2024-04-13 03:05:04 +0700

    add lesson 1

---
+++ lesson-1/index.js
@@ -0,0 +1,37 @@
+// --------- VARIABLE ----------
+
+
+// Old JS
+var name1 = 'John'
+
+// ES6
+let name2 = 'John'
+const name3 = 'John'
+
+
+// --------- DATA TYPES / TIPE DATA ----------
+// Primitive Data Types
+// String
+let name = 'John'
+// Number
+let age = 25
+// Boolean
+let isMarried = true
+// Null
+let isNull = null
+// Undefined
+let isUndefined = undefined
+
+// Reference Data Types
+// Array
+let hobbies = ['Music', 'Movie']
+// Object
+let address = {
+  city: 'Jakarta',
+  country: 'Indonesia'
+}
+
+// Function
+let getHobbies = function() {
+  return hobbies
+}
